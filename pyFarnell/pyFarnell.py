b5773c5ee5e19cd3275070116955b93598460f05
Marcel Hecko <marcel.hecko@moirelabs.com>
2017-01-03 17:55:03 +0000

    Initial test - lear how to make PIP package

---
+++ pyFarnell/pyFarnell.py
@@ -0,0 +1,19 @@
+import requests
+
+class Farnell():
+
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
+        self.base_url = 'https://api.element14.com/catalog/products'
+
+    def get_part_number(self, partNum):
+        params = {
+                   'callInfo.responseDataFormat': 'JSON',
+                   'term': 'id:' + str(partNum),
+                   'storeInfo.id': 'sk.farnell.com',
+                   'callInfo.apiKey': self.apiKey,
+                 }
+        r = requests.get(self.base_url, params = params)
+        if r.status_code != 200:
+            raise Exception("Developer apiKey not active - contact Farnell representative.")
+        return r.__dict__
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='pyFarnell',
+    version='0.1.0.dev4',
+    description='Connect to Farnell REST API from Python. This is absolutely NOT OFFICIAL package!!',
+    url='http://www.github.com/hecko/pyFarnell',
+    author='Marcel Hecko',
+    author_email='maco@blava.net',
+    classifiers=[
+                  'Development Status :: 3 - Alpha',
+                  'Programming Language :: Python :: 2.7',
+                ],
+    packages=find_packages(),
+    install_requires=['requests'],
+)
