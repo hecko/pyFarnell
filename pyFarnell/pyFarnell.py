31b0b5709dc9d6ee20b6d19fd3312818fd7ece7d
Marcel Hecko <marcel.hecko@moirelabs.com>
2017-01-04 13:57:39 +0000

    add region

--- pyFarnell/pyFarnell.py
+++ pyFarnell/pyFarnell.py
@@ -5,10 +5,13 @@ class Farnell():
 
     :param apiKey: Farnells Product Search API apiKey    
     :type apiKey: str
+    :param region: Region for querying information (effectively the country) (for example: 'uk.farnell.com', 'au.element14.com' or 'mexico.newark.com').
+        All the regions are listed here: http://partner.element14.com/docs/Product_Search_API_REST__Description#storeinfo
+    :type region: str
 
     """
 
-    def __init__(self, apiKey = ''):
+    def __init__(self, apiKey = '', region = 'sk.farnell.com'):
         if not apiKey:
             raise Exception("Contact Farnell representative to create an accout for \'Product Search API: Standard' product for you.")
         
