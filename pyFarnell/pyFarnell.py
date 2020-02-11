58738b7ede2f5d817337cc3a8e7916781cd2c691
Your Name <you@example.com>
2020-02-11 19:50:26 +0100

    allow params extension

--- pyFarnell/pyFarnell.py
+++ pyFarnell/pyFarnell.py
@@ -19,7 +19,7 @@ class Farnell():
         self.region = region
         self.base_url = 'https://api.element14.com/catalog/products'
 
-    def get_part_by_number(self, partNum):
+    def get_part_by_number(self, partNum, part = {}):
         """Gets part information
 
         :param partNum: Farnells part order number
@@ -35,6 +35,7 @@ class Farnell():
                    'storeInfo.id': self.region,
                    'callInfo.apiKey': self.apiKey,
                  }
+        params.update(part)
         r = requests.get(self.base_url, params = params)
         if r.status_code != 200:
             raise Exception("Developer apiKey not active - contact Farnell representative to activate account for \'Product Search API: Standard' product - API key " + str(self.apiKey))
